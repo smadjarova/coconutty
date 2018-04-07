96b6e35d9dbaa28c163c0429b18913c7a442dddb
Sophia Madjarova <sophie@dhcp-v880-22-02119.1xwireless.unc.edu>
2018-04-07 02:36:14 -0400

    added all coconut like objects

--- sprites-script.ts
+++ sprites-script.ts
@@ -13,34 +13,25 @@ document.body.appendChild(app.view);
 let background: Sprite = Sprite.fromImage("./beach.png");
 app.stage.addChild(background);
 
-let tajika: Sprite = Sprite.fromImage("./tajika2.png");
+let tajika: Sprite = Sprite.fromImage("./tajika1.png");
 tajika.scale.x = 1;
 tajika.scale.y = 1;
 tajika.x = 480;
 tajika.y = 230;
 app.stage.addChild(tajika);
 
-/*
-   You'll notice our goal (or cpuhat) is also a Sprite, we'll describe
-   how we're going to differentiate these objects later. 
-*/
-
-let coconutBrown: Sprite = Sprite.fromImage("./coconutBrown.png");
-coconutBrown.scale.x = .40;
-coconutBrown.scale.y = .40;
-coconutBrown.x = 450;
-coconutBrown.y = 200;
-app.stage.addChild(coconutBrown);
-
-// let coconutBomb: Sprite = Sprite.fromImage("./coconutBomb.png");
-// cpuhat.scale.x = 0.1;
-// cpuhat.scale.y = 0.1;
-// cpuhat.x = 450;
-// cpuhat.y = 240;
-// app.stage.addChild(cpuhat);
-
-
 window.onkeydown = (e: KeyboardEvent): void => {
+    
+    // if (e) {
+    //     let movement = true
+    //     if (movement) {
+    //      tajika = Sprite.fromImage("./tajika1.png");
+    //     } 
+    //     else {
+    //         tajika = Sprite.fromImage("./tajika2.png");
+    //     }
+    // }
+
     const LEFT: number = 37;
     const RIGHT: number = 39;
     const STEP: number = 20;
@@ -50,3 +41,52 @@ window.onkeydown = (e: KeyboardEvent): void => {
         tajika.x += STEP;
     }
 };
+
+class CoconutLikeObject {
+
+    numberOfCoconut: number;
+    typeOfCoconut: Sprite;
+
+    constructor(z: number, picture: string) {
+        this.numberOfCoconut = z;
+        this.typeOfCoconut = Sprite.fromImage(picture);
+        this.typeOfCoconut.scale.x = 0.40;
+        this.typeOfCoconut.scale.y = 0.40;
+    }
+}
+
+let coconutBrown = new CoconutLikeObject(0, "./coconutBrown.png");
+coconutBrown.typeOfCoconut.x = 450;
+coconutBrown.typeOfCoconut.y = 200;
+app.stage.addChild(coconutBrown.typeOfCoconut);
+
+let coconutBowlingBall = new CoconutLikeObject(1, "./coconutBowlingBall.png");
+coconutBowlingBall.typeOfCoconut.x = 400;
+coconutBowlingBall.typeOfCoconut.y = 200;
+app.stage.addChild(coconutBowlingBall.typeOfCoconut);
+
+let coconutBomb = new CoconutLikeObject(2, "./coconutBomb.png");
+coconutBomb.typeOfCoconut.x = 350;
+coconutBomb.typeOfCoconut.y = 200;
+app.stage.addChild(coconutBomb.typeOfCoconut);
+
+let coconutGolden = new CoconutLikeObject(2, "./coconutGolden.png");
+coconutGolden.typeOfCoconut.x = 300;
+coconutGolden.typeOfCoconut.y = 200;
+app.stage.addChild(coconutGolden.typeOfCoconut);
+
+let coconutPokeball = new CoconutLikeObject(2, "./coconutPokeball.png");
+coconutPokeball.typeOfCoconut.x = 250;
+coconutPokeball.typeOfCoconut.y = 200;
+app.stage.addChild(coconutPokeball.typeOfCoconut);
+
+
+// let coconutBomb: Sprite = Sprite.fromImage("./coconutBomb.png");
+// cpuhat.scale.x = 0.1;
+// cpuhat.scale.y = 0.1;
+// cpuhat.x = 450;
+// cpuhat.y = 240;
+// app.stage.addChild(cpuhat);
+
+
+
